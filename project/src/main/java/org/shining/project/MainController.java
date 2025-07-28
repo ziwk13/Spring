919f06d8a0e77f8cd182933e130d9d5ef693e566
ziwk13 <ziwk13@hanmail.net>
2025-07-28 15:00:49 +0900

    spring오후수업

---
+++ project/src/main/java/org/shining/project/MainController.java
@@ -0,0 +1,13 @@
+package org.shining.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+  @RequestMapping(value = "/")
+  public String main() {
+    return "main";
+  }
+}
